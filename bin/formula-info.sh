2dedf9169983cd928053a148ce9b782790cec740
Nat! <nat@mulle-kybernetik.com>
2018-02-19 23:49:51 +0100

    remove .bootstrap, remove bin, add mulle-project, remove templates

--- bin/formula-info.sh
+++
@@ -1,5 +0,0 @@
-PROJECT="mulle-sprintf"
-DESC="An extensible sprintf function supporting stdarg and mulle-vararg"
-DEPENDENCIES='${DEPENDENCY_TAP}mulle-buffer
-${DEPENDENCY_TAP}mulle-utf
-${DEPENDENCY_TAP}mulle-vararg'
