40d6e48e9b70a81c1aa093346dfffc85bc08eca2
Nat! <nat@mulle-kybernetik.com>
2020-02-01 16:14:57 +0100

    upgraded mulle-sde

--- .mulle/etc/env/environment-host-terodde.sh
+++ .mulle/etc/env/environment-host-terodde.sh
@@ -1,6 +1,6 @@
 #
 #
 #
-export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}:/home/src/srcO/mulle-c:/home/src/srcO/mulle-core:/home/src/srcO/mulle-concurrent:/home/src/srcO/mulle-objc:/home/src/srcO/MulleFoundation:/home/src/srcM/MulleEOF/EOControl:/home/src/srcM/MulleEOF/EOAccess"
+export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}:/mnt/d/srcM/MulleEOF/MulleEOAdaptor:/mnt/d/srcM/MulleEOF/MulleEOAccess:/mnt/d/srcM/MulleEOF/MulleEOControl:/mnt/d/srcO/MulleFoundation:/mnt/d/srcO/mulle-objc-ports:/mnt/d/srcO/mulle-objc:/mnt/d/srcO/mulle-core:/mnt/d/srcO/mulle-concurrent:/mnt/d/srcO/mulle-c"
 
 
